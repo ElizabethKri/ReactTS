3bede2b20fa029261b0edc941d4b52ba00c868c8
Elizabeth Kritskaya <post.kri@gmail.com>
2024-09-08 20:58:33 +0300

    add Controlled Input, Select, CheckBox Stories

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,5 @@
 import {ChangeEvent, useRef, useState} from 'react';
+import {action} from '@storybook/addon-actions';
 
 
 export default  {
@@ -6,7 +7,6 @@ export default  {
 }
 
 export const UncontrolledInput = () => <input/>
-
 export const TrackValueOfUncontrolledInput = () => {
     const [value, setValue] = useState('')
     const onChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -17,7 +17,6 @@ export const TrackValueOfUncontrolledInput = () => {
         <input value={value} onChange={onChange}/> - {value}
     </>
 }
-
 export const GetValueOfUncontrolledInputByButtonPress = () => {
     const [value, setValue] = useState('')
     const inputRef = useRef<HTMLInputElement>(null)
@@ -35,4 +34,30 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
     </>
 }
 export const ControlledInputWithFixedValue = () => <input value={"it-incubator.by"}/>
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState('')
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.value)
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckBox = () => {
+    const [parentValue, setParentValue] = useState(true)
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => setParentValue(e.currentTarget.checked)
+    return <input type={'checkbox'} checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('2')
+
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => setParentValue (e.currentTarget.value)
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Moscow</option>
+        <option value={'3'}>Spb</option>
+    </select>
+}
 
