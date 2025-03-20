ae7702cda6597ed07568e70bc9c10b1540bb53b9
Elizabeth Kritskaya <post.kri@gmail.com>
2025-03-20 13:17:06 +0300

    use useEffect

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,37 @@
+import {useEffect, useMemo, useState} from "react";
+
+export default  {
+    title: 'useEffect demo'
+}
+
+
+
+
+export const SimpleExample1 = () => {
+    console.log('Example')
+    const [fake, setFake] = useState(1)
+    const [counter, setCounter] = useState(1)
+    console.log('SimpleExample')
+
+    //при каждом рендере компоненты
+    useEffect(()=> {
+        console.log('useEffect')
+        document.title = counter.toString()
+    })
+
+    //запускается только при первом ренедере
+    useEffect(()=> {
+        console.log('useEffect')
+        document.title = counter.toString()
+    }, [])
+
+    //срабатывает в первый раз, когда сщгтеук поменялся
+    useEffect(()=> {
+        console.log('useEffect')
+        document.title = counter.toString()
+    }, [counter])
+    return <>
+        Hello, {counter} {fake}
+        <button onClick={() => setFake(prevState => prevState + 1)}>+</button>
+    </>
+}
